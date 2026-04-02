7b629e7a5f7f0ecf5b98eb2f785f95d232b72867
Lucas Negrão <lucas.negrao@gmail.com>
2026-04-02 04:43:22 -0300

    Update button.py

--- custom_components/universal_remote/button.py
+++ custom_components/universal_remote/button.py
@@ -1,1 +1,39 @@
 
+"""Support for ZHA button."""
+
+from __future__ import annotations
+
+import functools
+import logging
+
+from homeassistant.components.button import ButtonDeviceClass, ButtonEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
+from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
+
+# from .entity import ZHAEntity
+# from .helpers import (
+#     SIGNAL_ADD_ENTITIES,
+#     EntityData,
+#     async_add_entities as zha_async_add_entities,
+#     convert_zha_error_to_ha_error,
+#     get_zha_data,
+# )
+
+_LOGGER = logging.getLogger(__name__)
+
+class controllerButton(code, action):
+
+    def __init__(self, entity_data: EntityData) -> None:
+        """Initialize the ZHA binary sensor."""
+        super().__init__(entity_data)
+        if self.entity_data.entity.info_object.device_class is not None:
+            self._attr_device_class = ButtonDeviceClass(
+                self.entity_data.entity.info_object.device_class
+            )
+
+    async def async_press(self) -> None:
+        """Send out a update command."""
+        await self.entity_data.entity.async_press()
